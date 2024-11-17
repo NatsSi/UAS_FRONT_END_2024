6fff3afa8bf1b83c1f9922e787709722d6c5f70e
NatsSi <testamin90@gmail.com>
2024-11-17 19:54:14 +0700

    Connect MongoDB & UI Bootstrap 5

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -30,6 +30,11 @@ return [
     */
 
     'connections' => [
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_URI', 'mongodb+srv://NatSi:natankeren@clusterback-end.eqyq2np.mongodb.net/?retryWrites=true&w=majority&appName=ClusterBack-End'),
+            'database' => 'yogo',
+        ],
 
         'sqlite' => [
             'driver' => 'sqlite',
